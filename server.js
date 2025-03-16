b67be12ae98627d9a7ec091fce24fb3337e4b527
Mega-Crypto <m.cryptolotto@gmail.com>
2025-03-16 11:48:49 -0300

    Primeira versão do Mega Crypto

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Configurações básicas
 app.use(cors());
 app.use(express.json());
 
+// Rota básica
 app.get("/", (req, res) => {
-  res.send("Mega Crypto API está rodando! 🚀");
+  res.send("Mega Crypto está online! 🚀");
 });
 
-// Iniciar servidor
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
